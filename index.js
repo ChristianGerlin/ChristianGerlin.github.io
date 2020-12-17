007c9e890090fef8210208b83535c0b59633138a
ChristianGerlin <christiang@disroot.org>
2020-12-17 18:59:38 +0100

    .fixes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/Android/i))) {
-document.write('<link href="mobile.css"  rel="stylesheet">');
+document.write('<link href="/mobile.css"  rel="stylesheet">');
 } else {
-document.write('<link href="index.css"  rel="stylesheet">');
+document.write('<link href="/index.css"  rel="stylesheet">');
 }
 
 function panel(){
     window.location.href = "/panel/index.html";
+}
+
+function blogschreiben(){
+    window.location.href = "/panel/blog.html";
 }
