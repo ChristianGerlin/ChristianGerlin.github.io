de4db8f07a9e293557a9956a30274b726172b144
ChristianGerlin <christiang@disroot.org>
2021-01-02 22:02:55 +0100

    bug fixes with class active on weird .htmls

--- nav.js
+++ nav.js
@@ -1,6 +1,15 @@
 $("#nav-placeholder").load("/nav.html");
+var current = location.pathname.split("/")[1];
+
+if(current == "m.kontakt.html"){
+    current = "kontakt.html";
+}
+
+if(current == "m.twitter.timeline1.html" || current == "m.twitter.timeline2.html" || current == "m.twitter.timeline3.html"){
+    current = "twitter.html";
+}
 
 function isActive(){
-    var current = location.pathname.split("/")[1];
+    console.log(current);
     document.getElementById(current).classList.add('active');
 }
