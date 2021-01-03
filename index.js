252113f5ba49b0da2ba32170a356d6c895c85b74
ChristianGerlin <christiang@disroot.org>
2021-01-03 02:18:30 +0100

    #index.html active if accessed by domain

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/Android/i))) {
     document.write('<link href="/mobile.css"  rel="stylesheet">');
+
+    if(window.location.pathname == "/kontakt.html"){
+        window.location.href = "/m.kontakt.html";
+    }
 } else {
     document.write('<link href="/index.css"  rel="stylesheet">');
+
+    if(window.location.pathname == "/m.kontakt.html"){
+        window.location.href = "/kontakt.html";
+    }
 }
 
 function panel(){
@@ -17,5 +25,12 @@ function indexNoImgMobileSmall() {
         if((window.screen.availWidth)/(window.screen.availHeight) > 0.5){
             document.getElementById('indeximg').style.display="none";
         }
+    }
+}
+
+
+function isIndex(){
+    if(window.location.href == "https://christiangerlin.de/"){
+        document.getElementById('index.html').classList.add('active');
     }
 }
