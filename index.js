842674a643f942905006182b90fb08658e2a8aa7
ChristianGerlin <christiang@disroot.org>
2021-01-25 11:43:21 +0100

    added scrolling indicator

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ function blogschreiben(){
 function isIndex(){
     document.getElementById('index.html').classList.add('active');
 }
+
+document.onscroll = function() {
+        if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+            document.getElementById('scrollable').style.display='none';
+        }
+
+        if (window.innerHeight + window.scrollY < document.body.clientHeight) {
+            document.getElementById('scrollable').style.display='inherit';
+        }
+    }
